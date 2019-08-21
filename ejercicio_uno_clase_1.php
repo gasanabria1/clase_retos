b3fd6ec017237c378f2bed3338a3ead692ef2f96
gasanabria1 <22yz37mv7LMxu8D>
2019-08-21 12:25:51 -0500

    primera tarea

---
+++ ejercicio_uno_clase_1.php
@@ -0,0 +1,34 @@
+
+<?php
+//Este ejercicio devuelve lo que escribes usando la función echo.
+echo 'Hello';
+
+//En este ejercicio uso un ciclo for donde me escribe 10 mientras el contador sea inferior o igual a 10
+<?php
+for ($i = 1; $i <= 10; $i++) {
+    echo 10
+    .PHP_EOL;
+}
+
+//Este ejercicio te concatena 3 cadenas de texto usando .=
+<?php
+$cad1 = "Dani";
+
+$cad2 = "es";
+
+$cad3 = "programador";
+$cadt = $cad1 .= $cad2 .= $cad3;
+echo $cadt;
+
+
+//Este ejercicio te muestra 7 diferentes operaciones matemáticas básicas separadas por puntos y comas
+<?php
+$num ="72";
+echo $num+$num," ;".PHP_EOL;
+echo $num*$num," ;".PHP_EOL;
+echo $num-$num," ;".PHP_EOL;
+echo $num%$num," ;".PHP_EOL;
+echo $num/$num," ;".PHP_EOL;
+echo sqrt($num)," ;".PHP_EOL;
+echo pow($num,4)," ;".PHP_EOL;
+
